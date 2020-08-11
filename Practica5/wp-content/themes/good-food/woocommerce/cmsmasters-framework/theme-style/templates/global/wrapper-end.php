07ceaefa2084fc99d83a92cfd7d7079b4c0a8cfe
bryanled <36912510+bryanled@users.noreply.github.com>
2020-08-11 06:28:02 -0500

    'finallll'

--- Practica5/wp-content/themes/good-food/woocommerce/cmsmasters-framework/theme-style/templates/global/wrapper-end.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @cmsmasters_package 	Good Food
- * @cmsmasters_version 	1.0.4
- */
-
-
-echo '</div>' . "\n" . 
-'<!-- Finish Content -->' . "\n\n";
--- Practica5/wp-content/themes/good-food/woocommerce/cmsmasters-framework/theme-style/templates/loop/loop-start.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @cmsmasters_package 	Good Food
- * @cmsmasters_version 	1.0.4
- */
-
-
-?>
-<ul class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?> cmsmasters_products">

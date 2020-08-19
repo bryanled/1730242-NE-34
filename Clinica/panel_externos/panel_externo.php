067e07ef0ad2c7be1315b575f2e2d4cc9d37d888
bryanled <36912510+bryanled@users.noreply.github.com>
2020-08-18 20:42:57 -0500

    'web'

---
+++ Clinica/panel_externos/panel_externo.php
@@ -0,0 +1,9 @@
+<?php require_once "parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Página de Externos</h1>
+</div>
+<!--FIN del cont principal-->
+
+<?php require_once "parte_inferior.php"?>
